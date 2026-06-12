367180dc886a6d848c8a15b50735bae2de8af85a
rakesh <id4phn300@gmail.com>
2026-06-12 12:36:18 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,3 @@ export default function Home() {
     </main>
   );
 }
-export default function Home() {
-  return (
-    <main style={{ padding: "40px" }}>
-      <h1>Car Intel India</h1>
-      <p>Deployment working successfully.</p>
-    </main>
-  );
-}
